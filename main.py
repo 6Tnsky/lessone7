ba6a9bd96a747561276f8b18624fad2139d0d016
6Tnsky <n.6tnsky@gmail.com>
2024-10-24 19:29:47 +0300

    Add file

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import pickle
+
+
 #Создание базового класса Animal
 class Animal:
     def __init__(self, name, age):
@@ -52,6 +55,22 @@ class Zoo:
     def add_staff(self, staff_member):
         self.staff.append(staff_member)
 
+    def save_to_file(self, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump({'animals': self.animals, 'staff': self.staff}, file)
+        print("Zoo state has been saved.")
+
+    def load_from_file(self, filename):
+        try:
+            with open(filename, 'rb') as file:
+                data = pickle.load(file)
+                self.animals = data['animals']
+                self.staff = data['staff']
+            print("Zoo state has been loaded.")
+        except FileNotFoundError:
+            print("File not found. Starting with an empty zoo.")
+
+
 #Создадим классы ZooKeeper и Veterinarian.
 class Staff:
     def __init__(self, name):
@@ -65,28 +84,36 @@ class Veterinarian(Staff):
     def heal_animal(self, animal):
         print(f"{self.name} пора лечить {animal.name}.")
 
+# Создание зоопарка
+zoo = Zoo()
+
+# Попробуем загрузить состояние зоопарка из файла
+zoo.load_from_file('zoo_state.pkl')
 
 # Создание животных
-parrot = Bird("Попугай", 2, "Средний")
-tiger = Mammal("Tiger", 5, "Оранжевый")
-snake = Reptile("Питон", 3, "Гладкий")
+parrot2 = Bird("Ара", 2, "Большой>")
+tiger2 = Mammal("Lion", 5, "Оранжевый")
+snake2 = Reptile("Удав", 3, "Гладкий")
 
 # Создание сотрудников
-keeper = ZooKeeper("Жора")
-vet = Veterinarian("Доктор Лектор")
+#keeper = ZooKeeper("Жора")
+#vet = Veterinarian("Доктор Лектор")
 
 # Создание зоопарка и добавление животных и сотрудников
-zoo = Zoo()
-zoo.add_animal(parrot)
-zoo.add_animal(tiger)
-zoo.add_animal(snake)
+#zoo = Zoo()
+#zoo.add_animal(parrot2)
+#zoo.add_animal(tiger2)
+#zoo.add_animal(snake2)
 
-zoo.add_staff(keeper)
-zoo.add_staff(vet)
+#zoo.add_staff(keeper)
+#zoo.add_staff(vet)
+
+# Сохраним текущее состояние зоопарка в файл
+zoo.save_to_file('zoo_state.pkl')
 
 # Демонстрация полиморфизма
 animal_sound(zoo.animals)
 
 # Использование методов сотрудников
-keeper.feed_animal(tiger)
-vet.heal_animal(snake)
+#keeper.feed_animal(tiger)
+#vet.heal_animal(snake)
